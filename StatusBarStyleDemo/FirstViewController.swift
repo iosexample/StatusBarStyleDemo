9e6a51b515538b402c6fa1e99ea101dad182d0fc
Dong <donly@magicalboy.com>
2018-01-08 10:44:05 +0800

    Initial Commit

---
+++ StatusBarStyleDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StatusBarStyleDemo
+//
+//  Created by dong on 08/01/2018.
+//  Copyright © 2018 dong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
